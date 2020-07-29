e47e8d45c50c38093afe0e3f2d9f1dde84237459
JessicaLim8 <jessicalim@bell.net>
2020-07-29 13:38:50 -0400

    Add check for cloud-specific label

--- wordcloud/runner.rb
+++ wordcloud/runner.rb
@@ -8,6 +8,7 @@ class Runner
   REGEX_PATTERN = /\w[\w' ]+/
   ADDWORD = 'add'
   SHUFFLECLOUD = 'shuffle'
+  CLOUDTYPES = ['quarantine']
 
   def initialize(
     github_token:,
@@ -39,7 +40,7 @@ class Runner
       word = add_to_wordlist(word)
       generate_cloud
       message = "@#{@user} added '#{word}' to the Word Cloud"
-      # write to readme
+      octokit.add_label(label: CLOUDTYPES[-1])
     else
       comment = "Sorry, '#{@command}' is not valid. Please try again. Currently we support wordcloud|add|<your-word> and wordcloud|shuffle."
       octokit.error_notification(reaction: 'confused', comment: comment)
@@ -104,7 +105,7 @@ class Runner
   end
 
   def to_markdown
-    ReadmeGenerator.new(octokit: octokit).generate
+    ReadmeGenerator.new(octokit: octokit, label_list: CLOUDTYPES).generate
   end
 
   def acknowledge_issue
